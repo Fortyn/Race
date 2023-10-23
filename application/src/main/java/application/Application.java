4d2305ac89e85adf9b9eaa924dad74c816eadb39
Sorokin Georgii <Georgii_Sorokin1@epam.com>
2023-10-23 21:11:07 +0400

    prepare for tomcat (first try)

--- application/src/main/java/application/Application.java
+++ application/src/main/java/application/Application.java
@@ -3,14 +3,21 @@ package application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EntityScan("db.models")
-public class Application {
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
      SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
     }
 }
 
